4f29448aaabf6e16dbb00f1a45fdc9fa5d538043
Bjørn Egil Øygarden <89018956+oygarden@users.noreply.github.com>
2023-11-11 20:29:52 +0100

    Basics working

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# dat158-ml-assignment2
-DAT158 Machine Learning assignment #2
+# Location-based wallpaper generation
+
+This is a simple script that generates a wallpaper based on your location, time, and weather.
+
+## Usage
+
+To use the script you need to register an account with OpenAI and generate an API key. 
+
+The openai package uses the environment variable `OPENAI_API_KEY` to authenticate requests (unless you set it manually). See the [OpenAI API documentation](https://platform.openai.com/docs/quickstart/account-setup) for more information.
+
--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import geocoder
 import requests
+import os
+from datetime import datetime
 
 # Retrieve users geolocation
 geoloc = geocoder.ip('me')
@@ -36,22 +38,90 @@ wind_speed = weather['wind_speed']
 
 next_1_hour = response_json['properties']['timeseries'][1]['data']['next_1_hours']['summary']['symbol_code']
 
-weather_description = "User location: " + user_location + "\nTime: " + str(time) + "\nTemperature: " + str(temperature)+ "\nCloud area fraction: " + str(cloud_area_fraction) + "\nRelative humidity: " + str(relative_humidity) + "\nWind speed: " + str(wind_speed) + "\nNext hour: " + next_1_hour
+weather_description = "User location: " + user_location + "\nTime: " + str(time) + "\nTemperature: " + str(temperature)+ "\nCloud area fraction: " + str(cloud_area_fraction) + "\nRelative humidity: " + str(relative_humidity) + "\nWind speed: " + str(wind_speed) + "\nDescription: " + next_1_hour
 
 print(weather_description)
+
+# The user should be able to specify the style of the image to be generated
+user_preferences = "\nStyle: " + "Vincent van Gogh"
+
 
 # OpenAI
 from openai import OpenAI
 
 client = OpenAI()
 
-print(client.models.list())
-
-completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages= [
-        {"role": "user", "content": "Say this is a test."}
-    ]
+#API call for generating descriptive prompt for generation of wallpaper
+chat_completion = client.chat.completions.create(
+   model="gpt-3.5-turbo",
+   messages= [
+       {"role":"system", "content":"You generate a detailed description of the general atmosphere based on the weather forecast, location and user preferences to be used in image generation in 200 characters or less. Specify style and content of the image of your own choosing."},
+       {"role": "user", "content": "%s %s" % (weather_description, user_preferences)}
+   ]
 )
 
-print(completion.choices[0].message)
+# Retrieve prompt from API response
+prompt = chat_completion.choices[0].message.content
+print(prompt)
+
+# API call for generating wallpaper using dall-e-3 model
+image_completion = client.images.generate(
+    model="dall-e-3",
+    prompt=prompt,
+    size="1792x1024",
+    quality="standard",
+    n=1,
+)
+
+# Retrieve image URL from API response
+image_url = image_completion.data[0].url
+
+# Download image
+image = requests.get(image_url)
+
+# Define save directory
+save_directory_name = "images"
+save_directory = os.path.join(os.curdir, save_directory_name)
+
+# Create image name based on current date and time
+image_name = datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + ".png"
+
+# If directory does not exist, create it
+if not os.path.isdir(save_directory):
+    os.mkdir(save_directory)
+
+# Define image path
+image_path = os.path.join(save_directory, image_name)
+
+if image.status_code == 200:
+
+    with open(image_path, "wb") as image_file:
+        image_file.write(image.content)
+        image_file.close()
+        print(f'Image was saved as {image_name} in {save_directory_name} directory.')
+
+
+
+# Set wallpaper
+import os
+import ctypes
+import platform
+
+def set_wallpaper(image_name):
+    system = platform.system().lower()
+
+    path = ''
+
+    if system == 'windows':
+        path = os.getcwd() + '\\images\\' + image_name
+
+
+        # BOOL SystemParametersInfoW(
+        #    UINT  uiAction,
+        #    UINT  uiParam,
+        #    PVOID pvParam,
+        #    UINT  fWinIni
+        # );
+        ctypes.windll.user32.SystemParametersInfoW(20, 0, path, 0)
+
+set_wallpaper(image_name)
